3b0df96f3114b3349773cbeb54d28714d1bb9c43
Youssef Ben Yedder <86587007+Youssef04544@users.noreply.github.com>
2026-08-06 22:36:52 -0400

    Updated collision with raycast capsule
    Made wall hugging possible, and fixed the single ray glitch by replacing it with a capsule raycast that uses the player size to detect collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting.InputSystem;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float movSpeed = 7f;
     [SerializeField] GameInput gameInput;
     private bool isWalking = false;
+   
 
     // Update is called once per frame
     void Update()
@@ -14,8 +16,40 @@ public class Player : MonoBehaviour
 
         Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
 
+        float moveDistance = movSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f; //manually calculated in scene kinda
+        bool cantMove = Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight, playerRadius, movDir, moveDistance);
+
         isWalking = movDir != Vector3.zero;
-        transform.position += movDir * Time.deltaTime * movSpeed;
+        
+        if (cantMove)
+        {
+            //check if we can move on the X or Z so we dont block movement completely when trying to slide while wall hugging
+            Vector3 movDirX = new Vector3(movDir.x, 0, 0).normalized;
+            cantMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirX, moveDistance);
+
+            if (!cantMove)
+            {
+                movDir = movDirX;
+            }
+            else
+            {
+                Vector3 movDirZ = new Vector3(0,0,movDir.z).normalized;
+                cantMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirZ, moveDistance);
+                if (!cantMove)
+                {
+                    movDir = movDirZ;
+                }
+            }
+
+        }
+
+
+        if (!cantMove)
+        {
+            transform.position += movDir * moveDistance;
+        }
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, movDir, rotateSpeed*Time.deltaTime);
 
