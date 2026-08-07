724b346aae018a584bfd0e3c52d50c153a65913a
Youssef Ben Yedder <86587007+Youssef04544@users.noreply.github.com>
2026-08-07 06:08:36 -0400

    Some Code refactoring
    Also made animation transition instant, and allowed the player to rotate even when he cant move forward coz it felt better

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,52 +7,77 @@ public class Player : MonoBehaviour
     [SerializeField] private float movSpeed = 7f;
     [SerializeField] GameInput gameInput;
     private bool isWalking = false;
-   
+    private Vector3 lastInteractDirection = Vector3.zero;
 
     // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+        HandleInteract();
+    }
+
+    private void HandleInteract()
+    {
+        Vector2 inputVector = gameInput.InputVectorNormalized();
+
+        Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
+        
+
+        if (movDir != Vector3.zero)
+        {
+            lastInteractDirection = movDir;
+        }
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDirection,out RaycastHit raycastHit, interactDistance))
+        {
+            Debug.Log(raycastHit.transform);
+        }
+        else
+        {
+            Debug.Log("--");
+        }
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.InputVectorNormalized();
 
         Vector3 movDir = new Vector3(inputVector.x, 0, inputVector.y);
 
         float moveDistance = movSpeed * Time.deltaTime;
-        float playerRadius = 0.7f;
+        float playerRadius = 0.65f;
         float playerHeight = 2f; //manually calculated in scene kinda
-        bool cantMove = Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight, playerRadius, movDir, moveDistance);
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDir, moveDistance);
 
-        isWalking = movDir != Vector3.zero;
-        
-        if (cantMove)
+
+
+        if (!canMove)
         {
             //check if we can move on the X or Z so we dont block movement completely when trying to slide while wall hugging
             Vector3 movDirX = new Vector3(movDir.x, 0, 0).normalized;
-            cantMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirX, moveDistance);
+            canMove = movDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirX, moveDistance);
 
-            if (!cantMove)
+            if (canMove)
             {
                 movDir = movDirX;
             }
             else
             {
-                Vector3 movDirZ = new Vector3(0,0,movDir.z).normalized;
-                cantMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirZ, moveDistance);
-                if (!cantMove)
+                Vector3 movDirZ = new Vector3(0, 0, movDir.z).normalized;
+                canMove = movDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movDirZ, moveDistance);
+                if (canMove)
                 {
                     movDir = movDirZ;
                 }
             }
-
         }
-
-
-        if (!cantMove)
+        if (canMove)
         {
             transform.position += movDir * moveDistance;
         }
+        isWalking = movDir != Vector3.zero && canMove;
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, movDir, rotateSpeed*Time.deltaTime);
-
+        transform.forward = Vector3.Slerp(transform.forward, movDir, rotateSpeed * Time.deltaTime);
     }
 
     public bool IsWalking ()
