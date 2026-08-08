1c60648f12f0e83aecd662827cd1e480ba6c3f63
Youssef Ben Yedder <86587007+Youssef04544@users.noreply.github.com>
2026-08-08 06:28:22 -0400

    Added KitchenObject Parent Switching
    Added kitchen object functionality to know where it is at all times by making it set it's own parent and clear the old one when you move it

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,16 +1,63 @@
-using System;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
-    
+    [SerializeField] private bool testing;
+    [SerializeField] private ClearCounter targetClearCounter;
+
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject)
+            {
+                kitchenObject.SetClearCounter(targetClearCounter);
+            }
+        }
+    }
     public void Interact()
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
-        Debug.Log("interacted!");
+        if (!kitchenObject)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+            Debug.Log("interacted!");
+
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        if (kitchenObject)
+        {
+            kitchenObject = null;
+        }
+    }
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+    public Transform GetCounterTopPoint()
+    {
+        return counterTopPoint;
     }
 }
