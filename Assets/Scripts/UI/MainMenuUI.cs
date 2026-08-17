b1b743318545c57ba52292f290bf81744739f373
Youssef Ben Yedder <86587007+Youssef04544@users.noreply.github.com>
2026-08-16 20:05:32 -0400

    Add MainMenu+LoadingScene logic+UI
    add complete logic and UI for scene management between MainMenu to loadingscene to gamescene

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
